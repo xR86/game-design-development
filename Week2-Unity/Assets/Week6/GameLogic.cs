5502780e0b1925e0ab6cd56ad21b4568de0119a7
xR86 <9412441+xR86@users.noreply.github.com>
2017-06-17 11:55:53 +0300

    integrated unity week 6 project #1

---
+++ Week2-Unity/Assets/Week6/GameLogic.cs
@@ -0,0 +1,79 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+/*
+needs for FPSController
+http://answers.unity3d.com/questions/768615/detect-if-player-is-facing-an-object.html
+ */
+
+public class GameLogic : MonoBehaviour {
+    protected static List<string> match;
+    protected static List<GameObject> matchObjects;
+
+    private float lastFrameTime;
+    private float keypressDelta;
+
+    // Use this for initialization
+    void Start () {
+        match = new List<string> { };
+        lastFrameTime = Time.time;
+        keypressDelta = 0.2F;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        //if F is pressed and delta is passed (player doesn't mash the keys)
+        //check if combo is good (and give feedback)
+        if (Input.GetKey(KeyCode.F) && Time.time - keypressDelta > lastFrameTime)
+        {
+            print("F pressed !");
+
+            //If there is no element in the match
+            if (match.ElementAtOrDefault(0) == null)
+            {
+                print("Select a box !");
+            }
+            //If there is only one element in the match
+            else if (match.ElementAtOrDefault(1) == null)
+            {
+                print("Select another box !");
+            }
+            //If the match is good
+            else if (match[0] == match[1])
+            {
+                print("You had the correct match !");
+                Destroy(matchObjects[0]);
+                Destroy(matchObjects[1]);
+                print("Objects destroyed !");
+                
+                match.Clear();
+                matchObjects.Clear();
+                print("Combination cleared.");
+            }
+            //If the match is bad
+            else
+            {
+                print("Match is bad !");
+                match.Clear();
+                matchObjects.Clear();
+                print("Combination cleared.");
+            }
+
+            lastFrameTime = Time.time;
+            print("Time set: " + lastFrameTime);
+        }
+        //if R is pressed and delta is passed (player doesn't mash the keys)
+        //clear combo
+        else if (Input.GetKey(KeyCode.R) && Time.time - keypressDelta > lastFrameTime)
+        {
+            match.Clear();
+            matchObjects.Clear();
+            print("Combination cleared.");
+
+            lastFrameTime = Time.time;
+            print("Time set: " + lastFrameTime);
+        }
+    }
+}
---
+++ Week2-Unity/Assets/Week6/LetterBox.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using UnityEngine;
+
+public class LetterBox : GameLogic{
+    private string textContent;
+
+	// Use this for initialization
+	void Start () {
+        string chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+        int num = Random.Range(0, 4);
+
+        GameObject thisIniObject = this.transform.GetChild(0).gameObject;
+        TextMesh textObject = thisIniObject.GetComponent<TextMesh>();
+        
+        textObject.text = chars[num].ToString();
+        textContent = textObject.text;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    private void OnTriggerEnter(Collider other)
+    {
+        //Debug.Log(other.gameObject.name);
+
+        print("Object collided !");
+        print(other.gameObject.name);
+
+        //don't add to list if the combination overflowed
+        if(match.Count < 3 ) {
+            GameObject thisObject = this.transform.GetChild(0).gameObject;
+            
+            //if there is no element, add object
+            if (match.ElementAtOrDefault(0) == null)
+            {
+                match.Add(textContent);
+                matchObjects.Add(thisObject);
+            }
+            //if there is 1 element, add only if current gameobject is different
+            else if (matchObjects[0] != thisObject)
+            {
+                //condition: !GameObject.Equals(matchObjects[0], thisObject)
+                //condition: matchObjects[0] != thisObject
+                //print(thisObject.ToString());
+                //print(matchObjects[0].ToString());
+
+                match.Add(textContent);
+                matchObjects.Add(thisObject);
+            }
+            
+        }
+    }
+}
